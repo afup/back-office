e31fa7c772c51fa48404a9f81161d03ccbd26381
jewome62 <hello@jewome62.eu>
2014-11-17 00:31:16 +0100

    Fin design connexion

--- web/assets/js/login.js
+++ web/assets/js/login.js
@@ -2,69 +2,73 @@ var Login = function() {
 	"use strict";
 	var runBoxToShow = function() {
 		var el = $('.box-login');
-		if (getParameterByName('box').length) {
-			switch(getParameterByName('box')) {
-				case "register" :
-					el = $('.box-register');
-					break;
-				case "forgot" :
-					el = $('.box-forgot');
-					break;
-				default :
-					el = $('.box-login');
-					break;
-			}
-		}
+                var body = $('body');
+                
+		if (body.hasClass('register')) {
+                    el = $('.box-register');
+                } else if (body.hasClass('request')) {
+                    el = $('.box-forgot');
+                }
+                
 		el.show().addClass("animated flipInX").on('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
 			$(this).removeClass("animated flipInX");
 		});
 	};
 	var runLoginButtons = function() {
-		$('.forgot').on('click', function(event) {
+		$('.main-login').on('click','.forgot', function(event) {
                     
                     event.preventDefault();
                     
-                    $('.box-login').removeClass("animated flipInX").addClass("animated bounceOutRight").on('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-                            $(this).hide().removeClass("animated bounceOutRight");
-                    });
-
+                    $('.loading-forgot').show();
+                   
                     showModuleAjax($(this), '.box-forgot', function(){
+                        $('.box-login').removeClass("animated flipInX").addClass("animated bounceOutRight").on('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+                            $(this).hide().removeClass("animated bounceOutRight");
+                        });
+
+                        $('.loading-forgot').hide();
+                        
                         $('.box-forgot').show().addClass("animated bounceInLeft").on('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
                             $(this).show().removeClass("animated bounceInLeft");
                         });
                     });
 		});
-		$('.register').on('click', function(event) {
+		$('.main-login').on('click','.register', function(event) {
                     
-                     event.preventDefault();
+                    event.preventDefault();
                      
-                    $('.box-login').removeClass("animated flipInX").addClass("animated bounceOutRight").on('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-                            $(this).hide().removeClass("animated bounceOutRight");
-
-                    });
+                    $('.loading-register').show();
                     
                     showModuleAjax($(this), '.box-register', function(){
+                        $('.box-login').removeClass("animated flipInX").addClass("animated bounceOutRight").on('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+                            $(this).hide().removeClass("animated bounceOutRight");
+                        });
+                        
+                        $('.loading-register').hide();
+                        
                         $('.box-register').show().addClass("animated bounceInLeft").on('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
                             $(this).show().removeClass("animated bounceInLeft");
                         });
                     });
 
 		});
-		$('.go-back').click(function() {
-			var boxToShow;
-			if ($('.box-register').is(":visible")) {
-				boxToShow = $('.box-register');
-			} else {
-				boxToShow = $('.box-forgot');
-			}
-			boxToShow.addClass("animated bounceOutLeft").on('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-				boxToShow.hide().removeClass("animated bounceOutLeft");
+		$('.main-login').on('click','.go-back', function() {
+                    var boxToShow;
+                    if ($('.box-register').length !== 0 && $('.box-register').is(":visible")) {
+                            boxToShow = $('.box-register');
+                    } else {
+                            boxToShow = $('.box-forgot');
+                    }
+                    boxToShow.addClass("animated bounceOutLeft").on('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+                            boxToShow.hide().removeClass("animated bounceOutLeft");
 
-			});
-			$('.box-login').show().addClass("animated bounceInRight").on('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-				$(this).show().removeClass("animated bounceInRight");
-
-			});
+                    });
+  
+                    showModuleAjax($(this), '.box-login', function(){
+                        $('.box-login').show().addClass("animated bounceInRight").on('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+                            $(this).show().removeClass("animated bounceInRight");
+                        });
+                    });
 		});
 	};
         var showModuleAjax = function(click, className, callback) {
